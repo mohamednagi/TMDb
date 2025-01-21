98baaef170b29e5a04103431e310704380122aa2
mnagi <mnagi@youxel.com>
2025-01-21 08:27:37 +0200

    updated UseCase to fetch from repo

--- banquemisr.challenge05/Domain/UseCase/FetchMoviesUseCase.swift
+++ banquemisr.challenge05/Domain/UseCase/FetchMoviesUseCase.swift
@@ -8,19 +8,30 @@
 import Foundation
 
 protocol FetchMoviesUseCaseProtocol {
-    func FetchMovies(in query: MoviesListType) async
+    func fetchMovies(in query: MoviesListType) async
 }
 
 class FetchMoviesUseCaseImpl: FetchMoviesUseCaseProtocol {
     private let repo: FetchMoviesRepo
     
+    var nowPlayingMovies: DynamicObjects<[MoviesBaseModel]> = DynamicObjects([])
+    var popularMovies: DynamicObjects<[MoviesBaseModel]> = DynamicObjects([])
+    var upComingMovies: DynamicObjects<[MoviesBaseModel]> = DynamicObjects([])
+    
     init(repo: FetchMoviesRepo = FetchMoviesRepoImpl()) {
         self.repo = repo
     }
     
-    func FetchMovies(in query: MoviesListType) async {
+    func fetchMovies(in query: MoviesListType) async {
         do {
-            
+            switch query {
+            case .nowPlaying:
+                nowPlayingMovies.value = try await repo.fetchMovies(in: .nowPlaying)
+            case .popular:
+                popularMovies.value = try await repo.fetchMovies(in: .popular)
+            case .upcoming:
+                upComingMovies.value = try await repo.fetchMovies(in: .upcoming)
+            }
         } catch {
             fatalError("should be handled soon")
         }
